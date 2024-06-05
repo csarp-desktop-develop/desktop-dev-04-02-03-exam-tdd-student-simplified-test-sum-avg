d0143dd51d6d53989102bce0ae2701960dbab5a3
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-06-05 10:46:32 +0200

    Kész a feladat

--- Kreta.Desktop/Service/IAwardedStudentService.cs
+++ Kreta.Desktop/Service/IAwardedStudentService.cs
@@ -2,6 +2,15 @@
 {
     public interface IAwardedStudentService
     {
-        public int GetNumberOfAwardedStudent();
+        // 1. Azoknak a diákoknak akik egyszereri ösztöndíjat kapnak egy hónapban mennyi ösztöndíjat kell kifizetni?
+
+        // 2. Azoknak a diákoknak akik egyszereri ösztöndíjat kapnak egy hónapban mennyi a havi átlagos ösztöndíjuk egy tizedes jegyre kerekítve?
+
+        // 3. A adott tanévben szeptembertől decemberig mennyi ösztöndijat kell fizetni azoknak akik állandó (havi) ösztöndíjat kapnak?        
+
+        // 4. Az adott tanévben szeptembertől decemberig mennyi ösztöndijat kell fizetni az összes ösztöndíjassanak?
+
+        // 5. Az adott tanévben mennyi az állandó (havi) ösztöndíjjának átlaga két tizedes jegyre kerekítve szeptember hónapban!
+
     }
 }
---
+++ Kreta.DesktopTests/Service/AwardedStudentServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kreta.Desktop.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kreta.Desktop.Repos;
+using Moq;
+
+namespace Kreta.Desktop.Service.Tests
+{
+    [TestClass()]
+    public class AwardedStudentServiceTests
+    {
+        private readonly AwardedStudentService _awardedStudentService;
+        private readonly Mock<IAwardedStudentRepo> _awardedStudentRepoMock = new Mock<IAwardedStudentRepo>();
+
+        public AwardedStudentServiceTests()
+        {
+            _awardedStudentService = new AwardedStudentService(_awardedStudentRepoMock.Object);
+        }
+    }
+}
